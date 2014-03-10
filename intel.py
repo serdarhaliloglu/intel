06849a1c6beacabc0441adb09084379ffcce3314
Johnny C. Wachter <wachter.johnny@gmail.com>
2014-03-10 16:39:21 -0700

    Added ability to accept URL path as input.
    Previously, this program only extracted data from one or more input files; it now accepts fully qualified URLs as input also.

--- intel.py
+++ intel.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+
 """Extract and Validate NBIs and HBIs From Multiple Intel Sources."""
 
 from classes.Arguments import GetArguments  # Get and validate arguments.
-from classes.Data import CleanData  # Format input data to a python list.
+from classes import Data  # Format input data to a python list.
 from classes.FileHashes import ExtractHashes  # Extract hashes from input data.
 from classes.IPAddresses import ExtractIPs  # Extract IPs from input data.
 from classes.Domains import ExtractDomains  # Extract domains from input data.
@@ -54,10 +55,22 @@ def main():
 
     input_data = []  # List to store all input data to be analyzed.
 
-    for input_path in args.input:
+    for input_arg in args.input:
 
-        # Append input data to the list.
-        input_data.extend(CleanData(input_path).to_list())
+        input_path, path_type = input_arg  # Map tuple to variables.
+
+        if path_type == 'file_path':
+
+            # Append input data to the list.
+            input_data.extend(Data.CleanData(input_path).to_list())
+
+        elif path_type == 'url_path':
+
+            # Grab the HTML from the page. Read StringIO instance.
+            data = Data.GetData(input_path).get_url().getvalue()
+
+            # Append input data to the list.
+            input_data.extend(Data.CleanData(data).to_list())
 
     input_data = list(set(input_data))  # Cleaned up and deduped.
 
